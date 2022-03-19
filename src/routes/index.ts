8a4c12dc85e6eb96ee3c3ef0695c64e8c6d5b8e5
surya Vivek Janjam <suryavivek33377@gmail.com>
2022-03-19 20:57:01 +0530

    minor changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from '@sveltejs/kit';
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 export async function get() {
 	const users = await prisma.user.findMany();
