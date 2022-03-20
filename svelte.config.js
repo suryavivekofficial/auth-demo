636b30e77d49e5b12d55e502960e802cd8bcb3cc
surya Vivek Janjam <suryavivek33377@gmail.com>
2022-03-20 11:37:30 +0530

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
