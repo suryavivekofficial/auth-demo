d5e34b4e0eb72d58ef43dfe52794664570ac2820
surya Vivek Janjam <suryavivek33377@gmail.com>
2022-03-20 21:50:19 +0530

    teacher route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,13 @@
   Click here to go to login page
 </h1>
 </a>
+
+<a href="/teachers/dashboard">
+<h1 class="text-center my-8 hover:underline hover:text-blue-500">
+  Click here to go to teachers' dashboard
+</h1>
+</a>
+
 <h1 class="text-center my-12 text-2xl">Registered Users</h1>
 
 {#each users as user, index }
---
+++ src/routes/teachers/dashboard/index.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	const branches = ['All', 'CSE', 'ECE', 'EEE', 'Mech', 'Civil']
+	const years = ['All', 1, 2, 3, 4 ]
+</script>
+
+<div class="flex justify-center items-center flex-col gap-8">
+	<h1>Teacher's Dashboard</h1>
+	
+	<form  class="flex-col justify-center items-center flex gap-24">
+		<div class="flex gap-12">
+			<div class="">
+				<h3>Select branch</h3>
+				<select class="focus:outline-none p-4" name="Branch">
+					{#each branches as branch }
+						<option value={branch}>{branch}</option>
+					{/each}
+				</select>
+			</div>
+			
+			<div class="">
+				<h3>Select Year</h3>
+				<select class="focus:outline-none p-4" name="Year">
+					{#each years as year }
+						<option value={year}>{year}</option>
+					{/each}
+				</select>
+			</div>
+		</div>
+
+		<input type="submit" value="Create Test" class="py-3 px-10 cursor-pointer bg-black text-white rounded hover:text-black hover:bg-white duration-300 border-black border">
+	</form>
+</div>
