c1434eeb5adec5c1d408cdc321599104ee00e3e1
surya vivek <suryavivek33377@gmail.com>
2022-09-09 12:07:49 +0530

    removed unused components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,19 @@
 <script context="module">
 	export async function load({ fetch, session }) {
-		if(!session.user) {
+		if (!session.user) {
 			return {
-    			status: 303,
-    			redirect: '/login'
-    		}
+				status: 303,
+				redirect: '/login'
+			};
 		}
 
-    	const response = await fetch(`api/users/${session.user.id}`);
-    	const data = await response.json()
-    	const user = data.user
-    	if(user.role === 'teacher'){
-    		return {
-    			status: 303,
-    			redirect: '/teacher'
-    		}
-    	} else {
-    		return {
-    			status: 303,
-    			redirect: '/student'
-    		}
-    	}  	
-  	}
+		const response = await fetch(`api/users/${session.user.id}`);
+		const data = await response.json();
+		const user = data.user;
+
+		return {
+			status: 303,
+			redirect: '/profile'
+		};
+	}
 </script>
